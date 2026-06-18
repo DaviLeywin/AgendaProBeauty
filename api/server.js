58b8e7218d812bcdef565f397d493aea1cf0a548
DAVI ALMEIDA <davi.061208@bento.local>
2026-06-18 17:11:58 -0300

    nada demais

--- api/server.js
+++ api/server.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+import app from './app.js';
 const port = 3000;
-app.use(cors());
-app.use(express.json());
-
-
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
